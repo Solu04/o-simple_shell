2ea2ea814a5cb93a87b3e4f7b08901a782451dd1
some guy <yossef01102015@gmail.com>
2023-05-25 03:42:55 +0200

    Life is like a river, always flowing and changing. It's up to us to navigate its rocky rapids and make the most of our journey.

---
+++ builtin_commands3.c
@@ -0,0 +1,167 @@
+#include "shell.h"
+
+
+
+/**
+ * cmp_env_name - compares env variables names
+ * with the name passed.
+ * @nenv: name of the environment variable
+ * @name: name passed
+ *
+ * Return: 0 if are not equal. Another value if they are.
+ */
+int cmp_env_name(const char *nenv, const char *name)
+{
+	int i;
+
+	for (i = 0; nenv[i] != '='; i++)
+	{
+		if (nenv[i] != name[i])
+		{
+			return (0);
+		}
+	}
+
+	return (i + 1);
+}
+
+/**
+ * _getenv - get an environment variable
+ * @name: name of the environment variable
+ * @_environ: environment variable
+ *
+ * Return: value of the environment variable if is found.
+ * In other case, returns NULL.
+ */
+char *_getenv(const char *name, char **_environ)
+{
+	char *ptr_env;
+	int i, mov;
+
+	/* Initialize ptr_env value */
+	ptr_env = NULL;
+	mov = 0;
+	/* Compare all environment variables */
+	/* environ is declared in the header file */
+	for (i = 0; _environ[i]; i++)
+	{
+		/* If name and env are equal */
+		mov = cmp_env_name(_environ[i], name);
+		if (mov)
+		{
+			ptr_env = _environ[i];
+			break;
+		}
+	}
+
+	return (ptr_env + mov);
+}
+
+/**
+ * _env - prints the evironment variables
+ *
+ * @datash: data relevant.
+ * Return: 1 on success.
+ */
+int _env(data_shell *datash)
+{
+	int i, j;
+
+	for (i = 0; datash->_environ[i]; i++)
+	{
+		for (j = 0; datash->_environ[i][j]; j++)
+			;
+
+		write(STDOUT_FILENO, datash->_environ[i], j);
+		write(STDOUT_FILENO, "\n", 1);
+	}
+	datash->status = 0;
+
+	return (1);
+}
+
+
+/**
+ * check_error_cmd - verifies if user has permissions to access
+ *
+ * @dir: destination directory
+ * @datash: data structure
+ * Return: 1 if there is an error, 0 if not
+ */
+int check_error_cmd(char *dir, data_shell *datash)
+{
+	if (dir == NULL)
+	{
+		get_error(datash, 127);
+		return (1);
+	}
+
+	if (_strcmp(datash->args[0], dir) != 0)
+	{
+		if (access(dir, X_OK) == -1)
+		{
+			get_error(datash, 126);
+			free(dir);
+			return (1);
+		}
+		free(dir);
+	}
+	else
+	{
+		if (access(datash->args[0], X_OK) == -1)
+		{
+			get_error(datash, 126);
+			return (1);
+		}
+	}
+
+	return (0);
+}
+
+/**
+ * cmd_exec - executes command lines
+ *
+ * @datash: data relevant (args and input)
+ * Return: 1 on success.
+ */
+int cmd_exec(data_shell *datash)
+{
+	pid_t pd, wpd;
+	int state, exec;
+	char *dir;
+	(void)wpd;
+
+	exec = is_executable(datash);
+	if (exec == -1)
+		return (1);
+	if (exec == 0)
+	{
+		dir = _which(datash->args[0], datash->_environ);
+		if (check_error_cmd(dir, datash) == 1)
+			return (1);
+	}
+
+	pd = fork();
+	if (pd == 0)
+	{
+		if (exec == 0)
+			dir = _which(datash->args[0], datash->_environ);
+		else
+			dir = datash->args[0];
+		execve(dir + exec, datash->args, datash->_environ);
+	}
+	else if (pd < 0)
+	{
+		perror(datash->av[0]);
+		return (1);
+	}
+	else
+	{
+		do {
+			wpd = waitpid(pd, &state, WUNTRACED);
+		} while (!WIFEXITED(state) && !WIFSIGNALED(state));
+	}
+
+	datash->status = state / 256;
+	return (1);
+}
